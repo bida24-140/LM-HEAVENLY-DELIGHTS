36dfb71de63406b26c8151286156f868339b626a
bida24-140 <bida24-140@thuto.bac.ac.bw>
2025-05-25 12:03:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById("order").addEventListener("submit", function(event) {
   <link rel="stylesheet" href="styles.css">
 
     document.getElementById("Thank you for your order.We've recieved your order and will start preparing it shortly.").style.display = "block"; // Show confirmation message
+    <a href="order.html">Go back to the order form</a>
 
     // Optionally, clear the form
     this.reset();
