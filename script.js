924407dd844b2f1e8f4caafa985299b6ecf367d7
bida24-140 <bida24-140@thuto.bac.ac.bw>
2025-05-25 11:58:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById("orderForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent page reload
+  <link rel="stylesheet" href="styles.css">
 
     document.getElementById("Thank you for your order.We've recieved your order and will start preparing it shortly.").style.display = "block"; // Show confirmation message
 
