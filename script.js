83eacd49439cf349e68cad4b92db3a9f6a7d61e9
bida24-140 <bida24-140@thuto.bac.ac.bw>
2025-05-25 13:10:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const orderForm= document.getElementById("orderForm");
-
-    order.addEventListener("submit order", function(event) {
-        event.preventDefault(); // Prevent page refresh
-
-        // Redirect to the confirmation page after submitting the order
-        window.location.href = "confirmation.html";
-    });
+document.querySelector("#orderForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    window.location.href = "confirmation.html";
 });
+
 
